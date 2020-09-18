8ed78f5e6ee4798c215b048a6030fd699eac1b43
Nohier <nohierassan2@gmail.com>
2020-09-18 15:09:04 +0200

    Testing

--- app/api.py
+++ app/api.py
@@ -14,6 +14,7 @@ class Product(Resource):
                       'Chocolate', 
                       'Fruit',
                       'Vegies',
+                      'Others',
         
                   ]
     }
