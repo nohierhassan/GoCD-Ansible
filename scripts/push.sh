dc40ef956cba8e02ad2729721ea5e6817feb1a11
Nohier <nohierassan2@gmail.com>
2020-09-18 11:17:58 +0200

    Testing revision number is taging and pushing

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,1 +1,4 @@
 echo "Pushing .............." 
+REVISION_NUMBER=$1 
+
+echo "The revision number is $REVISION_NUMBER " 
--- scripts/tag.sh
+++ scripts/tag.sh
@@ -1,1 +1,4 @@
 echo "Taging ................" 
+REVISION_NUMBER=$1
+
+echo "The revision number is $REVISION_NUMBER"
